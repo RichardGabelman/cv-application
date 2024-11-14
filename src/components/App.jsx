8fe3104734e43f53aaedad02bdc79658920832a1
RichardGabelman <78068360+RichardGabelman@users.noreply.github.com>
2024-11-14 15:52:03 -0800

    Plan basic high level structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-import '../styles/App.css'
+import { useState } from "react";
+import "../styles/App.css";
 
 function App() {
-
+  // TODO: contact info
+  // TODO: education info
+  // TODO: practical info
   return (
     <>
+      <Contact />
+      <Education />
+      <Practical />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
