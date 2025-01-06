0f9f037699bf3076c799f288c0a11a4565eae0df
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-01-06 07:51:11 -0800

    Implement basic education section react component

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,7 +1,56 @@
 import { useState } from "react";
 
-function Education() {
-  return <></>;
+function Education({ submitted }) {
+  const [school, setSchool] = useState("");
+  const [study, setStudy] = useState("");
+  const [date, setDate] = useState("");
+
+  if (submitted) {
+    console.log("Submitted");
+
+    return (
+      <div id="educationSection">
+        <h2>{school}</h2>
+        <p>{study}</p>
+        <p>{date}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div id="educationSection">
+      <form>
+        <legend>Education</legend>
+
+        <label htmlFor="school">School: </label>
+        <input
+          type="text"
+          id="school"
+          name="school"
+          value={school}
+          onChange={(event) => setSchool(event.target.value)}
+        />
+
+        <label htmlFor="study">Study: </label>
+        <input
+          type="text"
+          id="study"
+          name="study"
+          value={study}
+          onChange={(event) => setStudy(event.target.value)}
+        />
+
+        <label htmlFor="date">Date: </label>
+        <input
+          type="text"
+          id="date"
+          name="date"
+          value={date}
+          onChange={(event) => setDate(event.target.value)}
+        />
+      </form>
+    </div>
+  );
 }
 
 export default Education;
